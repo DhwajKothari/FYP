c5313cea063fdbc071126f63ca7a23138c4d34e0
Sabito <yatint5@gmail.com>
2022-05-12 20:06:56 +0530

    Add files via upload

---
+++ PSNR_SSIM/psnr_ssim.py
@@ -0,0 +1,98 @@
+import math
+import numpy as np
+import cv2
+import argparse
+import os
+from math import log10, sqrt
+
+def ssim(img1, img2):
+    C1 = (0.01 * 255)**2
+    C2 = (0.03 * 255)**2
+
+    img1 = img1.astype(np.float64)
+    img2 = img2.astype(np.float64)
+    kernel = cv2.getGaussianKernel(11, 1.5)
+    window = np.outer(kernel, kernel.transpose())
+
+    mu1 = cv2.filter2D(img1, -1, window)[5:-5, 5:-5]  # valid
+    mu2 = cv2.filter2D(img2, -1, window)[5:-5, 5:-5]
+    mu1_sq = mu1**2
+    mu2_sq = mu2**2
+    mu1_mu2 = mu1 * mu2
+    sigma1_sq = cv2.filter2D(img1**2, -1, window)[5:-5, 5:-5] - mu1_sq
+    sigma2_sq = cv2.filter2D(img2**2, -1, window)[5:-5, 5:-5] - mu2_sq
+    sigma12 = cv2.filter2D(img1 * img2, -1, window)[5:-5, 5:-5] - mu1_mu2
+
+    ssim_map = ((2 * mu1_mu2 + C1) * (2 * sigma12 + C2)) / ((mu1_sq + mu2_sq + C1) *
+                                                            (sigma1_sq + sigma2_sq + C2))
+    return ssim_map.mean()
+
+
+def calculate_ssim(img1, img2):
+    '''calculate SSIM
+    the same outputs as MATLAB's
+    img1, img2: [0, 255]
+    '''
+    if not img1.shape == img2.shape:
+        raise ValueError('Input images must have the same dimensions.')
+    if img1.ndim == 2:
+        return ssim(img1, img2)
+    elif img1.ndim == 3:
+        if img1.shape[2] == 3:
+            ssims = []
+            for i in range(3):
+                ssims.append(ssim(img1, img2))
+            return np.array(ssims).mean()
+        elif img1.shape[2] == 1:
+            return ssim(np.squeeze(img1), np.squeeze(img2))
+    else:
+        raise ValueError('Wrong input image dimensions.')
+  
+def PSNR(original, compressed):
+    mse = np.mean((original - compressed) ** 2)
+    if(mse == 0):  # MSE is zero means no noise is present in the signal .
+                  # Therefore PSNR have no importance.
+        return 100
+    max_pixel = 255.0
+    psnr = 20 * log10(max_pixel / sqrt(mse))
+    return psnr
+"""  
+parser  = argparse.ArgumentParser()
+parser.add_argument("-p1", type=str)
+parser.add_argument("-p2", type=str)
+args = parser.parse_args()
+img1 = cv2.imread(args.p1, 0)
+img2 = cv2.imread(args.p2, 0) 
+print("SSIM",calculate_ssim(img1, img2))
+print("PSNR:", PSNR(img1, img2))
+"""
+
+low_res = []
+hr = None
+
+for file in os.listdir(os.getcwd()):
+    if file[0] == "l":
+        low_res.append(file)
+    elif file[-1] != 'y':
+        hr = file
+
+high = cv2.imread(hr)
+print()
+for lrf in low_res:
+    print("Comparing", lrf, "with HR (", hr, ")")
+    low = cv2.imread(lrf)
+    print("SSIM", calculate_ssim(low, high))
+    print("PSNR", PSNR(high, low)) 
+    print()
+
+
+
+
+
+
+
+
+
+
+
+
